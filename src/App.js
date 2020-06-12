d4872bb4695324fcb9baa91c14a86804a8f33e2a
Ike Njoku <ikeenjoku@gmail.com>
2020-06-12 18:48:17 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App () {
+
+  return (
+    <div>
+      <h1>Just-Notes</h1>
+      <p>Welcome to the Just-Note application</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
