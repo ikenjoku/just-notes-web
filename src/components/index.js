c0d14ad03aa1e3e75c537f2c946b89f8934245a9
Ike Njoku <ikeenjoku@gmail.com>
2020-06-16 02:46:44 +0100

    create notefeed component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 export {  default as Header } from './Header'
 export {  default as Layout } from './Layout'
 export {  default as Navigation } from './Navigation'
+export {  default as Note } from './Note'
+export {  default as NoteFeed } from './NoteFeed'
 
 export { Button } from './Button'
 export { default as CreateGlobalStyle } from './GlobalStyle'
